79c3b4087289331107ede85efdf50764c1e7d34d
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-05-29 11:00:13 +0300

    Update monitoring icon

--- src/util/react/SvgIcon.tsx
+++ src/util/react/SvgIcon.tsx
@@ -2,19 +2,22 @@ import * as React from "react";
 
 export interface ISvgIconProps {
     size?: number;
+    viewBoxSize?: number;
 }
 
 export class SvgIcon extends React.Component<ISvgIconProps> {
     static defaultProps = {
-        size: 24
+        size: 24,
+        viewBoxSize: 24
     };
 
     render() {
         let size = this.props.size!;
+        let viewBoxSize = this.props.viewBoxSize!;
 
         return (
             <svg width={size + "px"} height={size + "px"} style={{ display: "block" }}
-                viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`} version="1.1" xmlns="http://www.w3.org/2000/svg">
                 {this.props.children}
             </svg>
         );
