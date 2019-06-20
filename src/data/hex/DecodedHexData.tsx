a9595bace071f70a1db0d07be618e670afa62e63
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-06-20 11:47:15 +0300

    Fix clipboard copy confirmation for DecodedHexData

--- src/data/hex/DecodedHexData.tsx
+++ src/data/hex/DecodedHexData.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 import styled from "../../styled-components";
 import { TooltipRegular } from "../../overlay/tooltip/TooltipRegular";
-import { IconButton } from "../../control/IconButton";
 import { IClipboard } from "../IClipboard";
 import { hexToString } from "../../util/internal/hex";
 import { StringData } from "../StringData";
-import { CopyIcon } from "../../icon/CopyIcon";
-import { Clipboard } from "@puzzl/browser/lib/Clipboard";
+import { CopyValueButton } from "../../control/button/CopyValueButton";
 
 const CopyButtonWrapper = styled.div`
     margin: 0 8px 0 16px;
@@ -24,10 +22,9 @@ export class DecodedHexData extends React.PureComponent<IDecodedHexDataProps> {
                 <div style={{display: "flex", alignItems: "center"}}>
                     <div>{"0x" + data.replace(/^0x/, "")}</div>
                     <CopyButtonWrapper>
-                        <IconButton
-                            Icon={CopyIcon}
-                            color={theme => theme.colors.copyIcon}
-                            onClick={this.copyExtraData}
+                        <CopyValueButton
+                            value={"0x" + this.props.data.replace(/^0x/, "")}
+                            clipboard={this.props.clipboard}
                         />
                     </CopyButtonWrapper>
                 </div>
@@ -37,9 +34,5 @@ export class DecodedHexData extends React.PureComponent<IDecodedHexDataProps> {
                 </StringData>
             </TooltipRegular>
         );
-    }
-    private copyExtraData = () => {
-        let clipboard = this.props.clipboard || new Clipboard(document);
-        clipboard.copy("0x" + this.props.data.replace(/^0x/, ""));
     }
 }
