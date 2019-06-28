1b2fa83b5229897d554eded3c8af3dfb95933c5b
Elias Müller <elias.muller@consensys.net>
2019-06-28 12:31:12 +0200

    Adapted gird row y padding to make the rows the exact same height as before.

--- src/control/grid/internal/GridRow.tsx
+++ src/control/grid/internal/GridRow.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import styled from "../../../styled-components";
 
 const GRID_CELL_PADDING_X = 32;
+const GRID_CELL_PADDING_Y = 8;
 
 interface ISpacerProps {
     odd?: boolean;
@@ -16,7 +17,7 @@ interface IItemProps {
     className?: string;
 }
 const Item = styled<IItemProps, "div">("div")`
-    padding: 10px ${GRID_CELL_PADDING_X}px;
+    padding: ${GRID_CELL_PADDING_Y}px ${GRID_CELL_PADDING_X}px;
     background-color: ${({ odd, theme }) => odd ? theme.colors.gridOddRowBg : theme.colors.gridEvenRowBg};
 `;
 const VertBorder = styled.div`
