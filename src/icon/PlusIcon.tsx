fac64d6c2b990edf02b308e1a480af8ec2944525
Ciprian Banescu <ciprian.banescu@consensys.net>
2019-11-13 16:24:37 +0200

    added PlusIcon

---
+++ src/icon/PlusIcon.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { SvgIcon, ISvgIconProps } from "../util/react/SvgIcon";
+
+export interface IPlusIconProps extends ISvgIconProps {
+
+}
+
+export class PlusIcon extends React.Component<IPlusIconProps> {
+    render() {
+        return (
+            <SvgIcon {...this.props}>
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g>
+                        <polygon id="Shape" points="0 0 24 0 24 24 0 24"></polygon>
+                        <polygon id="Shape" fill="currentColor" fill-rule="nonzero"
+                            points="17 13 13 13 13 17 11 17 11 13 7 13 7 11 11 11 11 7 13 7 13 11 17 11">
+                        </polygon>
+                    </g>
+                </g>
+            </SvgIcon >
+        );
+    }
+}
