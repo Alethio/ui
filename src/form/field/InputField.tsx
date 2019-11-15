7af8f6975a8c85937c109fc97b1a08e01148c819
Ciprian Banescu <ciprian.banescu@consensys.net>
2019-11-15 16:01:15 +0200

    added icons for input field validation

--- src/form/field/InputField.tsx
+++ src/form/field/InputField.tsx
@@ -1,6 +1,9 @@
-import { Field, GenericFieldHTMLAttributes } from "formik";
 import React from "react";
+import { Field, GenericFieldHTMLAttributes, FieldAttributes } from "formik";
+import styled from "../../styled-components";
 import { Input } from "../../control/Input";
+import { ErrorIcon } from "../../icon/ErrorIcon";
+import { StatusOkIcon } from "../../icon/StatusOkIcon";
 
 export interface IInputFieldProps {
     name: string;
@@ -9,12 +12,33 @@ export interface IInputFieldProps {
     innerRef?(instance: any): void;
 }
 
+const FieldContainer = styled.div`
+    position: relative;
+`;
+
+const IconContainer = styled.div`
+    pointer-events: none;
+    position: absolute;
+    top: 6px;
+    right: 6px;
+    color: ${props => props.theme.colors.base.status.success};
+`;
+
 export class InputField extends React.Component<
     IInputFieldProps & GenericFieldHTMLAttributes & React.HTMLAttributes<HTMLInputElement>
-> {
+    > {
     render() {
         let { ...props } = this.props;
 
-        return <Field {...props} as={Input} />;
+        return <Field {...props} style={{ paddingRight: 36 }} >
+            {({ field, form }: FieldAttributes<any>) => {
+                return <FieldContainer>
+                    <Input {...field} />
+                    {(form.touched.email) && (form.errors.email ? <IconContainer><ErrorIcon /></IconContainer>
+                        : <IconContainer><StatusOkIcon /></IconContainer>
+                    )}
+                </FieldContainer>;
+            }}
+        </Field>;
     }
 }
