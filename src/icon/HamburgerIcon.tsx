a400d16396e135b97618528d80303f41fa3c27b1
Mircea Ulmeanu <mircea.ulmeanu@consensys.net>
2019-11-22 14:22:38 +0200

    Modified topbar height and hamburger menu

--- src/icon/HamburgerIcon.tsx
+++ src/icon/HamburgerIcon.tsx
@@ -5,13 +5,17 @@ export interface IHamburgerIconProps extends ISvgIconProps {
 
 }
 
+// tslint:disable: max-line-length
 export class HamburgerIcon extends React.Component<IHamburgerIconProps> {
     render() {
         return (
             <SvgIcon {...this.props}>
-                <rect x="0" y="4" width="24" height="3.61" fill="currentColor" fillRule="nonzero" />
-                <rect x="0" y="10.5" width="24" height="3.61" fill="currentColor" fillRule="nonzero" />
-                <rect x="0" y="17" width="24" height="3.61" fill="currentColor" fillRule="nonzero" />
+                <g stroke="none" strokeWidth="1" fill="currentColor" fillRule="evenodd">
+                    <g transform="translate(4.000000, 9.000000)">
+                        <path d="M0,7.27272727 L16,7.27272727 L16,6 L0,6 L0,7.27272727 L0,7.27272727 Z M0,4.27272727 L16,4.27272727 L16,3 L0,3 L0,4.27272727 L0,4.27272727 Z M0,0 L0,1.27272727 L16,1.27272727 L16,0 L0,0 L0,0 Z">
+                        </path>
+                    </g>
+                </g>
             </SvgIcon>
         );
     }
