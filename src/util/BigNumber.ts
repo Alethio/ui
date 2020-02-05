052286b13474036f6f6f3bbf21c952c0d526e3b6
Ciprian Banescu <ciprian.banescu@consensys.net>
2020-02-05 13:32:23 +0200

    Fix bignumber error when using TS 3.7

--- src/util/BigNumber.ts
+++ src/util/BigNumber.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line:import-blacklist
-import { BigNumber } from "bignumber.js";
+import { BigNumber as BN } from "bignumber.js";
 
 // We depend only the type side of bignumber.js, we should make the instance side available
-export type BigNumber = BigNumber;
-export type Format = BigNumber.Format;
+export type BigNumber = BN;
+export type Format = BN.Format;
