51efc97da17bf1cebfcffa32e40e2fb1ed1174c0
Ciprian Banescu <ciprian.banescu@consensys.net>
2020-02-07 17:37:02 +0200

    Update GridViewIcon & ListViewIcon (#71)

--- src/icon/GridViewIcon.tsx
+++ src/icon/GridViewIcon.tsx
@@ -13,7 +13,7 @@ export class GridViewIcon extends React.Component<IGridViewIconProps> {
                     <polygon points="0 0 24 0 24 24 0 24"></polygon>
                     <path d={
                         // tslint:disable-next-line:max-line-length
-                        "M4,11 L11,11 L11,4 L4,4 L4,11 Z M4,20 L11,20 L11,13 L4,13 L4,20 Z M13,20 L20,20 L20,13 L13,13 L13,20 Z M13,4 L13,11 L20,11 L20,4 L13,4 Z"
+                        "M11,9 L13,9 L13,7 L11,7 L11,9 Z M11,13 L13,13 L13,11 L11,11 L11,13 Z M11,17 L13,17 L13,15 L11,15 L11,17 Z M7,9 L9,9 L9,7 L7,7 L7,9 Z M7,13 L9,13 L9,11 L7,11 L7,13 Z M7,17 L9,17 L9,15 L7,15 L7,17 Z M15,7 L15,9 L17,9 L17,7 L15,7 Z M15,11 L15,13 L17,13 L17,11 L15,11 Z M15,15 L15,17 L17,17 L17,15 L15,15 Z"
                     } fill="currentColor" fillRule="nonzero"></path>
                 </g>
             </SvgIcon>
