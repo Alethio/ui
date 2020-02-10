1e0bf07efedb01329fa4546f3a70de8662c88aa5
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2020-02-10 12:16:16 +0200

    Fix GasUsedValueBox when gas limit is 0

--- src/data/box/GasUsedValueBox.stories.tsx
+++ src/data/box/GasUsedValueBox.stories.tsx
@@ -8,4 +8,7 @@ storiesOf("data/box/" + GasUsedValueBox.name, module)
     .addDecorator(storyFn => <div style={{ display: "flex" }}>{storyFn()}</div>)
     .add("default", () => (
         <GasUsedValueBox limit={new BigNumber(100000)} value={new BigNumber(40000)} locale="en-US"></GasUsedValueBox>
+    ))
+    .add("no limit", () => (
+        <GasUsedValueBox limit={new BigNumber(0)} value={new BigNumber(40000)} locale="en-US"></GasUsedValueBox>
     ));
