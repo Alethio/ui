aeac99b00f4e2aadc7adb89c8191ca52e5a51159
Mircea Ulmeanu <mircea.ulmeanu@consensys.net>
2020-02-17 15:05:30 +0200

    Fixed theme provider import

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { configure, addDecorator } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
-import { ThemeProvider } from "../src/styled-components";
+import { ThemeProvider } from "styled-components";
 import { createTheme } from "../src/theme/createTheme";
 import { createPalette } from "../src/theme/createPalette";
 
